66e3eed10b4ba288cd733d7e0645de5ec9f19d51
Neil Bateman <neilbatman@gmail.com>
2019-10-21 15:03:44 -0700

    add user validation schema with Joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const User = require('../model/User')
 
+//Validation
+const Joi = require('@hapi/joi');
+
+const schema = {
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required()
+}
+
 router.post('/register', async (req,res) => {
    const user = new User({
        name: req.body.name,
